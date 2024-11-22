92d933a01e771c9048de30307251e0cc68347587
Ethan <khoitranec@gmail.com>
2024-11-22 21:46:04 +0700

    03_reverseString completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(testString) {
+    let result = ''
+    for (let i = testString.length; i >= 0; i--) {
+        result += testString.charAt(i)
+    }
+    return result
 };
+
+
+
+
+reverseString('babyboo')
 
 // Do not edit below this line
 module.exports = reverseString;
