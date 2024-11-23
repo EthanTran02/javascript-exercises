fe1628701cfdbdcc1f26ba54e1b2f9dbbddd32dd
Ethan <khoitranec@gmail.com>
2024-11-23 22:43:56 +0700

    07_tempConversion completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) { 
+  let C = (F - 32) * 5/9
+  C = parseFloat(C.toFixed(1))
+  return C
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(-100))
+
+const convertToFahrenheit = function(C) {
+  F = (C * 9/5) + 32;
+  F = parseFloat(F.toFixed(1))
+  return F
 };
 
 // Do not edit below this line
