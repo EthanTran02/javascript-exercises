af15046b6089448d1b3e44a379ebfb3357ade3af
Ethan <khoitranec@gmail.com>
2024-12-01 00:13:58 +0700

    09 finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arrayForward = string.toLowerCase().split('')
+                                        .filter(item => item != ' ' 
+                                            && item != '!' 
+                                            & item != ',' && item != '.')
+    const arrayBackward = arrayForward.slice().reverse()
 
+        for (let i = 0; i < arrayForward.length; i++) {
+            if (arrayForward[i] == arrayBackward[i]) {
+                continue
+            } else {
+                return false
+            }
+        } return true
 };
 
 // Do not edit below this line
