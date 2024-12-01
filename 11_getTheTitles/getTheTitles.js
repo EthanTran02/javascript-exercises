206b1b3f72b40bf5bc3d8ace462f214b8486dac3
Ethan <khoitranec@gmail.com>
2024-12-01 14:10:01 +0700

    11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const getTitle = array.reduce((result, item) => {
+        result.push(item.title) 
+        return 'result'
+    }, [])
+    return getTitle
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+console.log(getTheTitles(books))
 
 // Do not edit below this line
 module.exports = getTheTitles;
