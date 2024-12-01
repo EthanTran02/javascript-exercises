c3a6269a8f12e4a7c8525b6de4a1036d4d73c184
Ethan <khoitranec@gmail.com>
2024-12-01 19:51:37 +0700

    11 fixed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,21 +1,12 @@
 const getTheTitles = function(array) {
     const getTitle = array.reduce((result, item) => {
         result.push(item.title) 
-        return 'result'
+        return result
     }, [])
     return getTitle
 };
 
-const books = [
-    {
-      title: 'Book',
-      author: 'Name'
-    },
-    {
-      title: 'Book2',
-      author: 'Name2'
-    }
-  ]
+
 console.log(getTheTitles(books))
 
 // Do not edit below this line
